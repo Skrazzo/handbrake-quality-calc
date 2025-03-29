98b93029e54fbabe2c08b8245a52e3cf858519b0
Leons Aleksandrovs <58330666+Skrazzo@users.noreply.github.com>
2025-03-29 17:25:06 +0200

    Update consts.ts

--- consts.ts
+++ consts.ts
@@ -4,16 +4,16 @@ import { join } from "path";
 export const OUTPUT_DIR = "C:\\Users\\Leons\\Desktop\\transcoded";
 export const CONVERT_TO = ".mp4";
 export const RANGE = {
-    min: 9.5,
-    max: 11,
+    min: 9,
+    max: 10,
 };
 export const PRESET_FILE = join(__dirname, "windows-preset.json");
 
 // When finding quality slider, this is how long video will be
-export const SECONDS = 3;
+export const SECONDS = 30;
 // This is how many times video will be split and transcoded
-export const SPLITS = 15;
+export const SPLITS = 5;
 
 // Binary search
-export const BINARY_QUALITY_RANGE = [40, 90];
-export const MAX_ITERATIONS = 8;
+export const BINARY_QUALITY_RANGE = [20, 40];
+export const MAX_ITERATIONS = 6;
